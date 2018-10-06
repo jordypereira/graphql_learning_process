debee9413d07a22ddb6c866c20ebd1b5e1e04596
Jordy Pereira <jordy.pereira@hotmail.com>
2018-10-06 02:23:03 +0200

    Add navbar, website links and accent color

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  title: "GraphQL Learning Process",
+  description:
+    "For school I had to learn a new technology. We have to document it and keep a logbook. I chose GraphQL and documented my whole process on this site.",
+  themeConfig: {
+    lastUpdated: "Last Updated",
+    repo: "perjor/graphql_learning_process",
+    nav: [
+      { text: "Home", link: "/" },
+      { text: "My Website", link: "https://jordypereira.be/" },
+    ],
+  },
+};
